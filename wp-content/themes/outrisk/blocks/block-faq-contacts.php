41d5ae9c05dc674491e306b8181788b929b6d151
Arlington Nyamukapa <info@quatrohaus.com>
2022-06-07 09:26:14 +0000

    faqs styles

---
+++ wp-content/themes/outrisk/blocks/block-faq-contacts.php
@@ -0,0 +1,25 @@
+<div class="faqBanner">
+    <div class="container">
+        <div class="faq-text">
+            <h3>Hello</h3>
+            <p>Nice of you to drop by. Our operating hours are 8:30am - 5pm Monday to Friday and we try to get to messages as fast as we can.</p>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <h4><?php block_field('chat-title'); ?></h4>
+                <p><?php block_field('chat-text'); ?></p>
+                <i class=""></i>
+            </div>
+            <div class="col-md-4">
+                <h4><?php block_field('email-title'); ?></h4>
+                <p><?php block_field('email-text'); ?></p>
+                <i class=""></i>
+            </div>
+            <div class="col-md-4">
+                <h4>Chat to us</h4>
+                <p>Get live support(during operating hours) from one of our Customer Heroes! Hit the Chat button</p>
+                <i class=""></i>
+            </div>
+        </div>
+    </div>
+</div>
--- wp-content/themes/outrisk/page-faqs.php
+++ wp-content/themes/outrisk/page-faqs.php
@@ -1,2 +1,14 @@
 <?php get_header(); ?>
+<?php if(have_posts()) :
+    
+    while(have_posts()) : the_post(); ?>
+    
+    <?php the_content(); ?>
+    
+   <?php endwhile; ?>
+
+    <?php wp_reset_postdata(); ?>
+    
+   <?php else: ?>    
+   <?php endif; ?>
 <?php get_footer(); ?>
